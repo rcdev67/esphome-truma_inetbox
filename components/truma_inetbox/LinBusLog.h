7530d2d2d4824710e234f6c8ef39baca42c458f4
rcdev67 <andreas@falmer.de>
2025-11-25 21:07:23 +0100

    Update LinBusLog.h
    Home Assistant 2025.11 changes

--- components/truma_inetbox/LinBusLog.h
+++ components/truma_inetbox/LinBusLog.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdint.h>              // wichtig für uint8_t
 #include "esphome/core/log.h"
 
 #define truma_log(_log_msg_) xQueueSend(this->log_queue_, (void *) &_log_msg_, QUEUE_WAIT_DONT_BLOCK);
@@ -66,9 +67,9 @@ enum class QUEUE_LOG_MSG_TYPE {
 // Log messages generated during interrupt are pushed to log queue.
 struct QUEUE_LOG_MSG {
   QUEUE_LOG_MSG_TYPE type;
-  u_int8_t current_PID;
-  u_int8_t data[9];
-  u_int8_t len;
+  uint8_t current_PID;     // statt u_int8_t
+  uint8_t data[9];         // statt u_int8_t
+  uint8_t len;             // statt u_int8_t
 #ifdef ESPHOME_LOG_HAS_VERBOSE
   bool current_data_valid;
   bool message_source_know;
