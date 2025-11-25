ddce4c8378fece3b8e05572f4ad76368e91f9185
rcdev67 <andreas@falmer.de>
2025-11-25 20:38:44 +0100

    Update __init__.py
    Home Assistant 2025.11 changes

--- components/truma_inetbox/number/__init__.py
+++ components/truma_inetbox/number/__init__.py
@@ -103,18 +103,18 @@ def set_default_based_on_type():
     return set_defaults_
 
 
-CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(TrumaNumber),
-        cv.GenerateID(CONF_TRUMA_INETBOX_ID): cv.use_id(TrumaINetBoxApp),
-        cv.Required(CONF_TYPE): cv.enum(CONF_SUPPORTED_TYPE, upper=True),
-        cv.Optional(CONF_MAX_VALUE): cv.float_,
-        cv.Optional(CONF_MIN_VALUE): cv.float_,
-        cv.Optional(CONF_STEP): cv.positive_float,
-    }
-).extend(cv.COMPONENT_SCHEMA)
-FINAL_VALIDATE_SCHEMA = set_default_based_on_type()
-
+CONFIG_SCHEMA = cv.All(
+    number.number_schema(TrumaNumber).extend(
+        {
+            cv.GenerateID(CONF_TRUMA_INETBOX_ID): cv.use_id(TrumaINetBoxApp),
+            cv.Required(CONF_TYPE): cv.enum(CONF_SUPPORTED_TYPE, upper=True),
+            cv.Optional(CONF_MAX_VALUE): cv.float_,
+            cv.Optional(CONF_MIN_VALUE): cv.float_,
+            cv.Optional(CONF_STEP): cv.positive_float,
+        }
+    ).extend(cv.COMPONENT_SCHEMA),
+    set_default_based_on_type(),
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
